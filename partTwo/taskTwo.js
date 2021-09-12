a9a9b00ee75e149486a434f4a5158fb5e11c1df7
HariisV <47668529+HariisV@users.noreply.github.com>
2021-09-12 20:00:15 +0700

    Update taskTwo.js

--- partTwo/taskTwo.js
+++ partTwo/taskTwo.js
@@ -6,11 +6,13 @@ const name = [
 'Ella', 'Faith', 'Olivia', 'Penelope']
 
 function Searching(keyword, limit, callbackProses, callbackCLG) {
-    let result = callbackProses(keyword, limit);
-    return callbackCLG(result)
+    console.log("Memulai")
+    let result = callbackProses(keyword, limit)
+    callbackCLG(result)
+    console.log("Selesai")
 }
 
-function callbackProses(keyword, limit){
+function prosesCallback(keyword, limit){
     let showData = [];
     let max = 0;
     name.forEach(element => {
@@ -27,8 +29,42 @@ function callbackProses(keyword, limit){
     return showData
 }
 
-function callbackCLG(result){
+function consoleCallback(result){
     console.log(result)
 }
 
-Searching("an", 3, callbackProses, callbackCLG)
+Searching("a", 5, prosesCallback, consoleCallback)
+
+// const name = [
+    // 'Abigail', 'Alexandra', 'Alison',
+    // 'Amanda', 'Angela', 'Bella',
+    // 'Carol', 'Caroline', 'Carolyn',
+    // 'Deirdre', 'Diana', 'Elizabeth',
+    // 'Ella', 'Faith', 'Olivia', 'Penelope']
+
+    // function Searching(keyword, limit) {
+    //     console.log("Memulai")
+
+    //     let showData = [];
+    //     setTimeout(() => {
+    //         let max = 0;
+    //         name.forEach(element => {
+    //             elementCheck = element.toLowerCase()
+    //             keyword = keyword.toLowerCase();
+
+    //             if (max >= limit) {
+    //                 return "selesai";
+    //             } else if (elementCheck.search(keyword) !== -1) {
+    //                 showData.push(element);
+    //                 max = max + 1;
+    //             }
+                
+    //         });
+    //         // console.log(showData)
+    //     }, 2000);
+    //     console.log(showData)
+    //     console.log("Selesai")
+    // }
+
+
+// Searching("an", 3)
